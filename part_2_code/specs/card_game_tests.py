e25a340c3ffa785904718835b767edd81003f2cf
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-01-06 00:17:38 +0000

    test 1 and 2 passed

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,19 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.card1 = Card("Spades", 3)
+        self.card2 = Card("Clubs", 1)
+
+        self.cards = [self.card1, self.card2]
+
+        self.card_game = CardGame()
+
+    def test_check_for_ace_function(self):
+        self.assertTrue(self.card_game.check_for_ace(self.card2))
+
+
+
+    def test_highest_card_function(self):
+        self.assertEqual(self.card1, self.card_game.highest_card(self.card1, self.card2))
